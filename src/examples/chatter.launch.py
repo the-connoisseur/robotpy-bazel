7b362f60f5c1804a1da0588f6c7a6fda7a972648
Sanjay Narayanan <sanjaynarayanan94@gmail.com>
2025-04-27 21:21:47 -0700

    Add a launch file to launch chatter apps together
    Ros 2's launch system lets us specify what programs to run, where to run
    them, what arguments to pass to them, etc. It is also responsible
    monitoring the state of the processes launched, and reporting and/or
    reacting to changes in the state of those processes.
    
    Add a simple launch file that just launches our `talker` and `listener`
    applications, and a Bazel target for it. This lets us spin up both nodes
    with the command:
    ```
    bazel run -c opt //src/examples:chatter
    ```

---
+++ src/examples/chatter.launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            executable = 'src/examples/talker',
+            name = 'talker'
+        ),
+        Node(
+            executable = 'src/examples/listener',
+            name = 'listener'
+        ),
+    ])
