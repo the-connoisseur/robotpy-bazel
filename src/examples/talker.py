6b829695d3c7f116ca16500860ef4ff06c849153
Sanjay Narayanan <sanjaynarayanan94@gmail.com>
2025-04-27 20:40:24 -0700

    Import Bazel rules for ROS 2 (#9)
    Pull in github.com/mvukov/rules_ros2. This seems to be the better of the
    available options, and also most actively maintained at the moment.
    Other options that were considered:
    * github.com/ApexAI/rules_ros
    * github.com/RobotLocomotion/drake-ros/bazel_ros2_rules
    
    This needs Python 3.11, so pull that in as well. Also set up a .bazelrc
    with options that are required for ROS applications to compile (the main
    one is C++17).
    
    Write a simple pair of applications (`talker` and `listener`) to show
    that this all works. They can be run in separate terminals to see
    publishing and subscribing in action.

---
+++ src/examples/talker.py
@@ -0,0 +1,29 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Talker(Node):
+    def __init__(self):
+        super().__init__('talker')
+        self.publisher = self.create_publisher(String, 'chatter', 10)
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello, world!'
+        self.publisher.publish(msg)
+        self.get_logger().info(f'Publishing: {msg.data}')
+
+def main():
+    rclpy.init()
+    talker = Talker()
+    try:
+        rclpy.spin(talker)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        talker.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
