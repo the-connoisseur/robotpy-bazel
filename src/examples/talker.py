6e4667f12f5049ac3dc2d59326244428d049f695
Sanjay Narayanan <sanjaynarayanan94@gmail.com>
2025-04-27 22:58:30 -0700

    Add an integration test for talker
    This type of test spins up any nodes that you specify and a test node
    for each test case.
    
    Change talker's publish rate to use a parameter. Set the parameter to a
    specific value for the test, and verify that it indeed publishes at that
    frequency.

--- src/examples/talker.py
+++ src/examples/talker.py
@@ -5,14 +5,18 @@ from std_msgs.msg import String
 class Talker(Node):
     def __init__(self):
         super().__init__('talker')
+        self.declare_parameter('timer_period_sec', 1.0)
+        self.timer_period_sec = self.get_parameter('timer_period_sec')
         self.publisher = self.create_publisher(String, 'chatter', 10)
-        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.timer = self.create_timer(self.timer_period_sec.value, self.timer_callback)
+        self.i = 0
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello, world!'
+        msg.data = f'Hello, world: {self.i}'
         self.publisher.publish(msg)
-        self.get_logger().info(f'Publishing: {msg.data}')
+        self.get_logger().debug(f'Publishing: {msg.data}')
+        self.i += 1
 
 def main():
     rclpy.init()
@@ -23,7 +27,9 @@ def main():
         pass
     finally:
         talker.destroy_node()
-        rclpy.shutdown()
+        # TODO: Uncomment the following line once the double shutdown bug is fixed.
+        # https://github.com/ros2/rclpy/issues/1081
+        # rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
---
+++ src/examples/talker_test.py
@@ -0,0 +1,63 @@
+import time
+import unittest
+
+import rclpy
+import launch_testing.markers
+import std_msgs.msg
+from launch import LaunchDescription
+from launch.actions import TimerAction
+from launch_ros.actions import Node
+from launch_testing.actions import ReadyToTest
+
+@launch_testing.markers.keep_alive
+def generate_test_description():
+    return LaunchDescription([
+        Node(
+            executable = 'src/examples/talker',
+            parameters = [
+                {
+                    'timer_period_sec': 0.01
+                }
+            ]
+        ),
+        # The talker node seems to take ~half a second to start publishing. Since our
+        # test cases expect talker to be in steady state (already publishing
+        # periodically), have the test node launch a full second after the talker to be
+        # on the safe side.
+        # TODO: Is there a more deterministic way to do this?
+        TimerAction(
+            period = 1.0,
+            actions = [
+                ReadyToTest()
+            ]
+        ),
+    ])
+
+class TalkerTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init()
+
+    @classmethod
+    def tearDownClass(cls):
+        rclpy.shutdown()
+
+    def setUp(self):
+        self.node = rclpy.create_node('talker_test')
+
+    def tearDown(self):
+        self.node.destroy_node()
+
+    def test_talker_publish_frequency(self, proc_output):
+        """Tests that the talker publishes at the frequency specified via the parameter."""
+        messages_rx = []
+        subscriber = self.node.create_subscription(std_msgs.msg.String, 'chatter', lambda message: messages_rx.append(message), 10)
+
+        try:
+            # Run for 1 second and verify that we got ~100 messages (once every 10ms).
+            end_time = time.time() + 1
+            while time.time() < end_time:
+                rclpy.spin_once(self.node, timeout_sec=0.02)
+            self.assertGreater(len(messages_rx), 100)
+        finally:
+            self.node.destroy_subscription(subscriber)
